6852114376dd0e20c21992375ef7640202e8c1c4
“theosap7” <theosap2002@gmail.com>
2024-05-31 15:02:28 +0300

    Name change

--- TicketSystem_CortexM0+ - Copy/PROJECT/main.d
+++
@@ -1,8 +0,0 @@
-.\main.o: src\main.c
-.\main.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\MKL25Z4.H
-.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm0plus.h
-.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_version.h
-.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_compiler.h
-.\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.8.0\CMSIS\Core\Include\cmsis_armcc.h
-.\main.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_KLxx_DFP\1.15.1\Device\Include\system_MKL25Z4.h
